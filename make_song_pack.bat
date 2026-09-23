88148e080bf4f6bdbe0e143bc0bba7fc71ee9a92
aki1050sun-art <aki1050sun@yahoo.co.jp>
2026-09-23 21:26:23 +0900

    Preconvert source audio before Demucs for reliable Windows loading

--- make_song_pack.bat
+++ make_song_pack.bat
@@ -23,20 +23,28 @@ set "WORK=%~dp1BandPracticeWork"
 set "OUTDIR=%WORK%\separated"
 set "PACKDIR=%WORK%\pack_%NAME%"
 set "ZIP=%~dp1%NAME%_bandmix.zip"
+set "CLEANWAV=%WORK%\input_clean.wav"
+
+if not exist "%WORK%" mkdir "%WORK%"
 
 echo.
-echo [1/4] Separating into 6 stems...
-"%PY%" -m demucs -n htdemucs_6s -o "%OUTDIR%" "%SRC%"
-if errorlevel 1 goto :error
-
-set "STEMDIR=%OUTDIR%\htdemucs_6s\%NAME%"
-if not exist "%STEMDIR%\vocals.wav" goto :error
-
-echo.
-echo [2/4] Converting WAV stems to MP3...
+echo [0/5] Preparing audio...
 for /f "usebackq delims=" %%F in (`"%PY%" -c "import imageio_ffmpeg; print(imageio_ffmpeg.get_ffmpeg_exe())"`) do set "FFMPEG=%%F"
 if not exist "%FFMPEG%" goto :error
 
+"%FFMPEG%" -y -loglevel error -i "%SRC%" -ar 44100 -ac 2 -c:a pcm_s16le "%CLEANWAV%"
+if errorlevel 1 goto :error
+
+echo.
+echo [1/5] Separating into 6 stems...
+"%PY%" -m demucs -n htdemucs_6s -o "%OUTDIR%" "%CLEANWAV%"
+if errorlevel 1 goto :error
+
+set "STEMDIR=%OUTDIR%\htdemucs_6s\input_clean"
+if not exist "%STEMDIR%\vocals.wav" goto :error
+
+echo.
+echo [2/5] Converting WAV stems to MP3...
 if exist "%PACKDIR%" rmdir /s /q "%PACKDIR%"
 mkdir "%PACKDIR%"
 
@@ -47,18 +55,18 @@ for %%S in (vocals guitar bass drums piano other) do (
 )
 
 echo.
-echo [3/4] Creating metadata...
+echo [3/5] Creating metadata...
 powershell -NoProfile -Command "$o=[ordered]@{title='%NAME%';bpm=120;countBars=1}; $o | ConvertTo-Json | Set-Content -Encoding UTF8 '%PACKDIR%\metadata.json'"
 if errorlevel 1 goto :error
 
 echo.
-echo [4/4] Creating ZIP song pack...
+echo [4/5] Creating ZIP song pack...
 if exist "%ZIP%" del /q "%ZIP%"
 powershell -NoProfile -Command "Compress-Archive -Path '%PACKDIR%\*' -DestinationPath '%ZIP%' -Force"
 if errorlevel 1 goto :error
 
 echo.
-echo DONE
+echo [5/5] DONE
 echo %ZIP%
 echo.
 echo Copy this ZIP to your Android phone and import it in Band Practice Mixer.
